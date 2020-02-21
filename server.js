49c3a2139edb43b6627f1f2ba7f2ca88135f0a00
melkam beyene <melkam3392beyu@gmail.com>
2020-02-21 11:19:38 +0300

    add routes

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
-const express=require('express');
-const app=express();
-const connectDB=require('./config/database')
-const path=require('path');
+const express = require("express");
+const app = express();
+const connectDB = require("./config/database");
+const path = require("path");
 
-connectDB();
+// connectDB();
 
-if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'));
-  
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
-  
-  const port=process.env.PORT || 3000;
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
 
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
-app.listen(port,()=>{
-    console.log(`The app is running on port${port}`);
-})
+const port = process.env.PORT || 3000;
+
+// Main Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
+
+app.listen(port, () => {
+  console.log(`The app is running on port  ${port}`);
+});
