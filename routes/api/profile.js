81b956f399465a7e0147f188c4bf2b7b666fb14d
melkam beyene <melkam3392beyu@gmail.com>
2020-02-27 12:39:21 +0300

    add profile model

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
-
+const Profile = require("../../models/profile");
+const auth = require("../../middleware/auth");
 router.get("/", (req, res) => {
   res.json("Profile routes");
 });
 
+router.post("/addProfile", auth, (req, res) => {});
 module.exports = router;
--- server.js
+++ server.js
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 
 // Main Routes
 app.use("/api/users", require("./routes/api/users"));
